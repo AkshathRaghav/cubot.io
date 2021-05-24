36039756208bffe16bb33faac94f93adba07f1f0
Akshath Raghav <75845563+AkshathRaghav@users.noreply.github.com>
2021-05-25 03:20:28 +0530

    Add files via upload

---
+++ src/Checker.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList ;
+public class Checker extends Moves {
+
+    public Checker(String[] s) throws IllegalArgumentException {
+        super(s) ;
+    }
+
+    //shorten - shortens the solution returned
+    public String shorten(String s ) { }
+
+
+
+    //converts String to alg ( to shorten the work ) - makes use of choose(ch)
+    public String stringalg(String str) {  }
+
+    //public method to solve and return solution
+    public String solve() {  }
+
+    public boolean isSolved() {
+        return cubie.solved(end) ;
+    }
+
+    public String[][][][] getSolvedCube() { return end.getCube() ; }
+
+    public ArrayList<String> compareToSolved() { }
+
+}
+
+
+
