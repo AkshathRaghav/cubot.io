0dbcbb55776c9d161d2ca01270787cf2162f9150
Akshath Raghav <akshathraghav.r@gmail.com>
2021-06-22 10:05:39 +0530

    Add files via upload

---
+++ src/Driver3.java
@@ -0,0 +1,17 @@
+public class Driver3 {
+    public static void main(String[] args) {
+        String[] temp = {"RRRR", "GGGG", "OOOO", "BBBB", "WWWW", "YYYY"};
+        Cubot cube = new Cubot(temp) ;
+        System.out.println(cube.cubeToString());
+//        String[] temp2 = {"RRRRRRRRR", "GGGGGGGGG", "OOOOOOOOO", "BBBBBBBBB", "WWWWWWWWW", "YYYYYYYYY"};
+//        cube = new Cubot(temp2) ;
+//        cube.stringalg("R") ;
+//        cube.stringalg("R'") ;
+//        System.out.println(cube);
+//        System.out.println(cube.isSolved());
+//        System.out.println(cube.compareToSolved());
+//        String[][][][] cubearr = cube.getCube() ;
+//        System.out.println(cube.indexString()) ;
+
+    }
+}
