4f6ebea2c5c90917c9a6314dd8fbace4fc44a81e
Akshath Raghav <akshathraghav.r@gmail.com>
2021-07-01 21:02:33 +0530

    Update Cubot2.java

--- src/Cubot2.java
+++ src/Cubot2.java
@@ -11,9 +11,9 @@ public class Cubot2 extends Checker2 {
     }
     public String[][][][] getSolvedCube() { return super.getSolvedCube() ; }
     public ArrayList<String> compareToSolved()  { return super.compareToSolved() ; }
-    public String stringAlg(String s) { return super.stringalg(s) ; }
+    public String stringAlg(String str) { return super.stringAlg(str) ; }
     public String getScramble(int n, boolean check) { return super.getScramble(n, check) ; }
-    public String reverseAlg(String s, boolean check ) { return  super.reversealg(s, check ) ; }
+    public String reverseAlg(String str, boolean check ) { return  super.reverseAlg(str, check ) ; }
     public String indexString() { return cubie.indexString();}
     public String solve() { return "Coming soon" ; }
     public String[] cubeToArr() { return cubie.cubeToArr() ; }
